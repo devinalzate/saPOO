465bafa173ee53058d4afcfd5fa9987eb623bbb2
Devin Alzate <devinalzate2005@gmail.com>
2023-11-18 20:56:54 -0500

    Quite el archivo raro de jhon

---
+++ proyecto_final_poo/src/co/edu/udistrital/controller/AplMain.java
@@ -0,0 +1,19 @@
+package co.edu.udistrital.controller;
+
+
+
+/**
+ *Esta es la clase dedicada a ejecutar el programa
+ * @author DEVIN ALZATE - SERGIO MENDIVELOS - JHON O'MEARA
+ */
+public class AplMain {
+
+    /**
+     * Metodo encargado de ejecutar el programa
+     * @param args: argumetos y retornos del programa
+     */
+    public static void main(String[] args) {
+        Controller control = new Controller();
+    }
+    
+}
---
+++ proyecto_final_poo/src/co/edu/udistrital/controller/Controller.java
@@ -0,0 +1,14 @@
+
+package co.edu.udistrital.controller;
+
+/**
+ *
+ * @author DEVIN ALZATE
+ */
+public class Controller {
+
+    public Controller() {
+        System.out.println("saPOO");
+    }
+    
+}
